e66e539db2857b6387c511bc8dcc909217aadb8a
Tmasters1581 <tmasters.ankit@gmail.com>
2019-01-26 07:42:27 +0530

    removeing unsed code

--- TMFramework/src/test/java/testScripts/Module3.java
+++ TMFramework/src/test/java/testScripts/Module3.java
@@ -25,22 +25,4 @@ public class Module3 extends BaseClass
 	{
 		System.out.println("Executing TC301");
 	}
-	
-	@Test
-	public void TC302()
-	{
-		System.out.println("Executing TC302");
-	}
-	
-	@Test
-	public void TC303()
-	{
-		System.out.println("Executing TC303");
-	}
-	
-	@Test
-	public void TC304()
-	{
-		System.out.println("Executing TC304");
-	}
 }
