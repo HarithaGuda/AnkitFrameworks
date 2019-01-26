1ed126ee5a9e9a29a46b4cb6eb76df936a663468
Tmasters1581 <tmasters.ankit@gmail.com>
2019-01-26 08:51:44 +0530

    Moduel2:Removed Unused Testcases TC202,TC203,TC204

--- TMFramework/src/test/java/testScripts/Module2.java
+++ TMFramework/src/test/java/testScripts/Module2.java
@@ -11,30 +11,4 @@ public class Module2 extends BaseClass
 	{
 		System.out.println("Executing TC201");
 	}
-	@Test
-	public void TC202()
-	{
-		System.out.println("Executing TC202");
-	}
-	@Test
-	public void TC203()
-	{
-		System.out.println("Executing TC203");
-	}
-	@Test
-	public void TC204()
-	{
-		System.out.println("Executing TC204");
-	}
-	
-	@Test
-	public void TC206()
-	{
-		System.out.println("Executing TC206");
-   }
-   @Test
-	public void TC205()
-	{
-		System.out.println("Executing TC205");
-	}
 }
