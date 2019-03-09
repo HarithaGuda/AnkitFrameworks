90f422f7175e4acc92e396eef72abaa6eee219cb
Tmasters1581 <tmasters.ankit@gmail.com>
2019-03-09 17:03:06 +0530

    modified login page

--- TMFramework/src/test/java/applicationPages/Pg101_Login.java
+++ TMFramework/src/test/java/applicationPages/Pg101_Login.java
@@ -1,5 +1,6 @@
 package applicationPages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,39 +13,41 @@ public class Pg101_Login extends BasePage
 	
 	//Initilization
 	WebDriver driver;
+	boolean stepstatus;
 	public Pg101_Login(WebDriver driver)
 	{
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+	
 	//define webelementss
 	//addeing commnet  to application login page
 	@FindBy(how=How.XPATH,using="//input[@name='txtUsername']")
 	WebElement Username;
-	
-	@FindBy(how=How.XPATH,using="//input[@name='txtPassword']")
-	WebElement Password;
-	
-	@FindBy(how=How.XPATH,using="//input[@name='Submit']")
-	WebElement Singin;
-	
-	
-	//related methods to perform operation on webelements
-	boolean stepstatus;
 	public void setUserName(String Strusername)
 	{
 		stepstatus = basepage.setText(Username, Strusername);
 		reportEvent(stepstatus, "Able to enter username", "Unable to enter username",driver);
 	}
+	
+	
+	@FindBy(how=How.XPATH,using="//input[@name='txtPassword']")
+	WebElement Password;
 	public void setpassword(String Strpassword)
 	{
 		stepstatus = basepage.setText(Password, Strpassword);
 		reportEvent(stepstatus, "Able to enter password", "Unable to enter password",driver);
 	}
 	
+	
+	public  WebElement Singin(String loginname)
+	{
+		return driver.findElement(By.xpath(String.format("//input[@name='%s']",loginname)));
+	}
 	public void ClickonLogin()
 	{
-		stepstatus = basepage.clickElement(Singin);
+		stepstatus = basepage.clickElement(Singin("Submit"));
 		reportEvent(stepstatus, "Able to click on Sign-In BUtton", "Unable to click on Sign-In Button",driver,true);
 	}
 }
